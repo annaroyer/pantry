c70d0d7727ceec9d652235685e8f371208bdd7e3
Anna Royer <anna.royer@colorado.edu>
2018-01-09 09:47:14 -0700

    add iteration 3 tests; begin iteration 3 methods

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,12 +1,14 @@
 class Pantry
   attr_reader :stock,
-              :shopping_list
+              :shopping_list,
+              :cookbook
 
   def initialize
     @stock = {}
     @stock.default = 0
     @shopping_list = {}
     @shopping_list.default = 0
+    @cookbook = []
   end
 
   def stock_check(ingredient)
@@ -31,4 +33,21 @@ class Pantry
     puts printed_list
     return printed_list
   end
+
+  def add_to_cookbook(recipe)
+    @cookbook << recipe
+  end
+
+  def can_make?(recipe)
+    recipe.ingredient_types.all? do |ingredient|
+      @stock[ingredient] >= recipe.amount_required(ingredient)
+    end
+  end
+
+  def what_can_i_make
+    @cookbook.find_all do |recipe|
+      recipe.ingredient_types
+
+  end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -81,4 +81,91 @@ class PantryTest < Minitest::Test
 
     assert_equal "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10\n", pantry.print_shopping_list
   end
+
+  def test_it_has_a_cookbook_that_is_empty_before_adding_recipes
+    pantry = Pantry.new
+
+    assert pantry.cookbook
+    assert pantry.cookbook.empty
+  end
+
+  def test_it_can_add_recipes_to_its_cookbook
+    pantry = Pantry.new
+    assert_equal 0, pantry.cookbook.length
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    assert_equal 1, pantry.cookbook.length
+
+    pantry.add_to_cookbook(r2)
+    assert_equal 2, pantry.cookbook.length
+
+    pantry.add_to_cookbook(r3)
+    assert_equal 3, pantry.cookbook.length
+  end
+
+  def test_what_can_i_make_returns_a_list_of_recipes_you_can_make_with_ingredients_in_stock
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Cucumbers", 120)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    assert_equal ["Pickles", "Peanuts"], pantry.what_can_i_make
+  end
+
+  def test_how_many_can_i_make_returns_recipes_and_how_many_of_each_you_can_make_with_ingredients_in_stock
+    pantry = Pantry.new
+
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Cucumbers", 120)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    assert_equal ({"Pickles" => 4, "Peanuts" => 2}), pantry.how_many_can_i_make
+  end
 end
